ab1ff1783c8be77a1dc84b54bd374607ca2170da
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-24 19:08:45 +0530

    Added Category Model

---
+++ nexus-api/src/main/java/com/nexus/retail/model/Category.java
@@ -0,0 +1,27 @@
+package com.nexus.retail.model;
+
+public class Category {
+    private Long categoryID;
+    private String categoryName;
+
+    public Category(Long categoryID, String categoryName) {
+        this.categoryID = categoryID;
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(Long categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
