b0d5b36cced1ecee6b2244e26d5f81273e5c77e1
nikhisable5050 <nikhilsable5050@gmail.com>
2026-03-24 18:22:37 +0530

    Added Spring Boot backend (nexus-api)

---
+++ nexus-api/src/main/java/com/nexus/retail/NexusApiApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusApiApplication.class, args);
+	}
+
+}
