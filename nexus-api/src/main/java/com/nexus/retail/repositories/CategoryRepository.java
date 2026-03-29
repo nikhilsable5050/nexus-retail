a4d9c66efdbf43e328249dd3792664ef63969b3e
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-03-29 14:00:41 +0530

    refactor(repository): update CategoryRepository with custom query methods

--- nexus-api/src/main/java/com/nexus/retail/repositories/CategoryRepository.java
+++ nexus-api/src/main/java/com/nexus/retail/repositories/CategoryRepository.java
@@ -4,4 +4,5 @@ import com.nexus.retail.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByCategoryName(String categoryName);
 }
