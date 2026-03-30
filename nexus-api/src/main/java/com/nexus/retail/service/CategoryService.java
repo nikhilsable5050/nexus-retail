68c9e085737fa37eaf47d75151caff643072380c
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-03-30 17:25:05 +0530

    feat: add pagination support to category APIs

--- nexus-api/src/main/java/com/nexus/retail/service/CategoryService.java
+++ nexus-api/src/main/java/com/nexus/retail/service/CategoryService.java
@@ -5,7 +5,7 @@ import com.nexus.retail.payload.CategoryDTO;
 import com.nexus.retail.payload.CategoryResponse;
 
 public interface CategoryService {
-    CategoryResponse getAllCategories();
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
     CategoryDTO createCategory(CategoryDTO categoryDTO);
 
     CategoryDTO deleteCategory(Long categoryId);
