4d502793a68131883694792cc5eb60305a0471e9
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-04-03 18:53:24 +0530

    feat: add ProductService and ProductServiceImpl

---
+++ nexus-api/src/main/java/com/nexus/retail/service/ProductService.java
@@ -0,0 +1,4 @@
+package com.nexus.retail.service;
+
+public interface ProductService {
+}
---
+++ nexus-api/src/main/java/com/nexus/retail/service/ProductServiceImpl.java
@@ -0,0 +1,7 @@
+package com.nexus.retail.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+}
