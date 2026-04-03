509fd837362beee782a9fb034b729ea6643e166a
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-04-03 18:51:59 +0530

    feat: add basic ProductController with base API mapping

---
+++ nexus-api/src/main/java/com/nexus/retail/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.nexus.retail.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+}
