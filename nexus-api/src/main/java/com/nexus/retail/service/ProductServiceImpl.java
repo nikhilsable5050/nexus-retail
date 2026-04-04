af1810759e24d896e8de60e86727f9c8dbc71952
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-04-04 18:13:16 +0530

    feat: implement add product business logic

--- nexus-api/src/main/java/com/nexus/retail/service/ProductServiceImpl.java
+++ nexus-api/src/main/java/com/nexus/retail/service/ProductServiceImpl.java
@@ -1,7 +1,40 @@
 package com.nexus.retail.service;
 
+import com.nexus.retail.exceptions.ResourceNotFoundException;
+import com.nexus.retail.model.Category;
+import com.nexus.retail.model.Product;
+import com.nexus.retail.payload.ProductDTO;
+import com.nexus.retail.repositories.CategoryRepository;
+import com.nexus.retail.repositories.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public ProductDTO addProduct(Long categoryId, Product product) {
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Category","categoryId",categoryId));
+
+        product.setImage("default.png");
+        product.setCategory(category);
+        double specialPrice = product.getPrice() -
+                ((product.getDiscount() * 0.01) * product.getPrice());
+        product.setSpecialPrice(specialPrice);
+        Product savedProduct = productRepository.save(product);
+        return modelMapper.map(savedProduct, ProductDTO.class);
+    }
 }
