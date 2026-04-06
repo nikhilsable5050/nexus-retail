ceecc8e69038e598472eabf8bbbeffa79bb44d3f
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-04-06 13:08:45 +0530

    feat: add validation annotations to Product and Category models

--- nexus-api/src/main/java/com/nexus/retail/model/Product.java
+++ nexus-api/src/main/java/com/nexus/retail/model/Product.java
@@ -1,6 +1,8 @@
 package com.nexus.retail.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +16,14 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name must be at least 3 characters long")
     private String productName;
     private String image;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must be at least 6 characters long")
     private String description;
     private Integer quantity;
     private double price;
