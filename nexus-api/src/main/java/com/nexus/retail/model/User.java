3be47ac062b2482258b786b1ace45b55794c2e71
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-04-09 20:13:08 +0530

    spelling correction

--- nexus-api/src/main/java/com/nexus/retail/model/User.java
+++ nexus-api/src/main/java/com/nexus/retail/model/User.java
@@ -52,7 +52,7 @@ public class User {
     @Getter
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
                 fetch = FetchType.EAGER)
-    @JoinTable(name = "user_roles",
+    @JoinTable(name = "user_role",
                 joinColumns = @JoinColumn(name = "user_id"),
                inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
