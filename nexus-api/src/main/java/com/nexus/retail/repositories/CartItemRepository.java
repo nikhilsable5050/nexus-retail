17cdd86be81b0a537b664bdc66c80302cc36781f
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-05-04 10:48:52 +0530

    Updated cart repositories with required query methods

--- nexus-api/src/main/java/com/nexus/retail/repositories/CartItemRepository.java
+++ nexus-api/src/main/java/com/nexus/retail/repositories/CartItemRepository.java
@@ -2,6 +2,9 @@ package com.nexus.retail.repositories;
 
 import com.nexus.retail.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
+    CartItem findCartItemByProductIdAndCartId(Long cartId, Long productId);
 }
--- nexus-api/src/main/java/com/nexus/retail/repositories/CartRepository.java
+++ nexus-api/src/main/java/com/nexus/retail/repositories/CartRepository.java
@@ -2,6 +2,9 @@ package com.nexus.retail.repositories;
 
 import com.nexus.retail.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    @Query("SELECT c FROM Cart c WHERE c.user.email = ?1")
+    Cart findCartByEmail(String email);
 }
