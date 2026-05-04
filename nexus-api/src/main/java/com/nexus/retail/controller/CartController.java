1d04e1aa196d115696636f2a80d51f528edd4fdf
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-05-04 10:49:20 +0530

    Added cart controller with endpoint to add product to cart

---
+++ nexus-api/src/main/java/com/nexus/retail/controller/CartController.java
@@ -0,0 +1,27 @@
+package com.nexus.retail.controller;
+
+import com.nexus.retail.payload.CartDTO;
+import com.nexus.retail.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/carts/products/{productId}/quantity/{quantity}")
+    public ResponseEntity<CartDTO> addProductToCart(@PathVariable Long productId,
+                                                    @PathVariable Integer quantity){
+        CartDTO cartDTO = cartService.addProductToCart(productId, quantity);
+        return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
+    }
+}
+
