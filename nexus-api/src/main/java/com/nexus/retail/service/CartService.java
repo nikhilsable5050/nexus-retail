c5d6c18df6885d8b1f7d32076360562adfd5922c
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-05-06 15:34:04 +0530

    Implemented update quantity functionality in cart module

--- nexus-api/src/main/java/com/nexus/retail/service/CartService.java
+++ nexus-api/src/main/java/com/nexus/retail/service/CartService.java
@@ -1,6 +1,7 @@
 package com.nexus.retail.service;
 
 import com.nexus.retail.payload.CartDTO;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
@@ -10,4 +11,7 @@ public interface CartService {
     List<CartDTO> getAllCarts();
 
     CartDTO getCart(String emailId, Long cartId);
+
+    @Transactional
+    CartDTO updateProductQuantityInCart(Long productId, Integer quantity);
 }
